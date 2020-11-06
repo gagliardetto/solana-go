b88026d7924a7c4e9dab2293e3094a2f1578b51b
Julien Cassis <juliencassis@gmail.com>
2020-11-06 17:02:52 -0500

    Added base and quote mint to market

--- serum/market.go
+++ serum/market.go
@@ -1,27 +1,21 @@
 package serum
 
 import (
-	"bytes"
-	"context"
 	"encoding/json"
 	"fmt"
-	"net/rpc"
 	"os"
-	"testing"
 
-	"github.com/lunixbochs/struc"
-	"github.com/stretchr/testify/require"
+	"github.com/dfuse-io/solana-go/token"
 
 	"github.com/dfuse-io/solana-go"
 )
 
 type MarketMeta struct {
-	Address               solana.PublicKey `json:"address"`
-	Name                  string           `json:"name"`
-	Deprecated            bool             `json:"deprecated"`
-	ProgramID             solana.PublicKey `json:"programId"`
-	BaseSPLTokenDecimals  uint64           `json:"base_spl_token_decimals"`
-	QuoteSPLTokenDecimals uint64           `json:"quote_spl_token_decimals"`
+	Address    solana.PublicKey `json:"address"`
+	Name       string           `json:"name"`
+	Deprecated bool             `json:"deprecated"`
+	QuoteMint  *token.Mint
+	BaseMint   *token.Mint
 
 	m *MarketV2
 }
@@ -40,22 +34,4 @@ func KnownMarket() ([]*MarketMeta, error) {
 		return nil, fmt.Errorf("unable to decode known markets: %w", err)
 	}
 	return markets, nil
-}
-
-func FetchMarket() {
-
-}
-
-func getMarket(t *testing.T, marketAddr solana.PublicKey) *MarketV2 {
-	c := rpc.NewClient("http://api.mainnet-beta.solana.com:80/rpc")
-	pubKey := solana.MustPublicKeyFromBase58("7xLk17EQQ5KLDLDe44wCmupJKJjTGd8hs3eSVVhCx932")
-	accInfo, err := c.GetAccountInfo(context.Background(), pubKey)
-	require.NoError(t, err)
-
-	accountData, err := accInfo.Value.DataToBytes()
-	require.NoError(t, err)
-
-	var m MarketV2
-	require.NoError(t, struc.Unpack(bytes.NewReader(accountData), &m))
-	return &m
 }
--- serum/rpc.go
+++ serum/rpc.go
@@ -5,6 +5,8 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/dfuse-io/solana-go/token"
+
 	"github.com/dfuse-io/solana-go"
 	"github.com/dfuse-io/solana-go/rpc"
 	"github.com/lunixbochs/struc"
@@ -33,14 +35,45 @@ func (s *SerumClient) FetchMarket(ctx context.Context, marketAddr solana.PublicK
 	}
 
 	var m MarketV2
-	err = struc.Unpack(bytes.NewReader(accountData), *m)
+	err = struc.Unpack(bytes.NewReader(accountData), &m)
 	if err != nil {
 		return nil, fmt.Errorf("unable to decode market: %w", err)
 	}
 
+	baseMint, err := s.getToken(ctx, m.BaseMint)
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve base token: %w", err)
+	}
+
+	quoteMint, err := s.getToken(ctx, m.QuoteMint)
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve base token: %w", err)
+	}
+
 	return &MarketMeta{
-		Address: marketAddr,
-		m:       m,
+		Address:   marketAddr,
+		QuoteMint: quoteMint,
+		BaseMint:  baseMint,
+		m:         &m,
 	}, nil
 
+}
+
+func (s *SerumClient) getToken(ctx context.Context, mintPubKey solana.PublicKey) (*token.Mint, error) {
+	accInfo, err := s.GetAccountInfo(ctx, mintPubKey)
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve base mint: %w", err)
+	}
+
+	accountData, err := accInfo.Value.DataToBytes()
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve account data byte: %w", err)
+	}
+
+	var m token.Mint
+	err = struc.Unpack(bytes.NewReader(accountData), &m)
+	if err != nil {
+		return nil, fmt.Errorf("unable to decode market: %w", err)
+	}
+	return &m, nil
 }
