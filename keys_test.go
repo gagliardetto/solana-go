7add73860def561279165da65a890d983eaef8a2
Matthieu Vachon <matt@dfuse.io>
2020-12-15 19:44:16 -0500

    Added key for hand crafted market

--- keys_test.go
+++ keys_test.go
@@ -1,11 +1,48 @@
 package solana
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func TestPublicKeyFromBase58(t *testing.T) {
+	emptyKey := PublicKey{}
+
+	tests := []struct {
+		name        string
+		in          string
+		expected    PublicKey
+		expectedErr error
+	}{
+		{
+			"hand crafted",
+			"SerumkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
+			MustPublicKeyFromBase58("SerumkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"),
+			nil,
+		},
+		{
+			"hand crafted error",
+			"SerkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
+			emptyKey,
+			errors.New("invalid length, expected 32, got 30"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual, err := PublicKeyFromBase58(test.in)
+			if test.expectedErr == nil {
+				require.NoError(t, err)
+				assert.Equal(t, test.expected, actual)
+			} else {
+				assert.Equal(t, test.expectedErr, err)
+			}
+		})
+	}
+}
 
 func TestPrivateKeyFromSolanaKeygenFile(t *testing.T) {
 	tests := []struct {
