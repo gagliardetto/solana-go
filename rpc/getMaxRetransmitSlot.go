b995cb08a5b56ebb6b38ea51412edb86b31460b8
Slavomir <gagliardetto@users.noreply.github.com>
2021-07-01 17:21:12 +0200

    Add RPC/getMaxRetransmitSlot

---
+++ rpc/getMaxRetransmitSlot.go
@@ -0,0 +1,11 @@
+package rpc
+
+import (
+	"context"
+)
+
+// GetMaxRetransmitSlot returns the max slot seen from retransmit stage.
+func (cl *Client) GetMaxRetransmitSlot(ctx context.Context) (out uint64, err error) {
+	err = cl.rpcClient.CallFor(&out, "getMaxRetransmitSlot")
+	return
+}
