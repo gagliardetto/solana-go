8435d331f31519ea132d2bb4eed425d06f8d39f3
Slavomir <gagliardetto@users.noreply.github.com>
2021-07-01 22:13:21 +0200

    Add RPC/getSlotLeader

---
+++ rpc/getSlotLeader.go
@@ -0,0 +1,22 @@
+package rpc
+
+import (
+	"context"
+
+	"github.com/dfuse-io/solana-go"
+)
+
+type GetSlotLeaderResult struct{}
+
+// GetSlotLeader returns the current slot leader,
+func (cl *Client) GetSlotLeader(
+	ctx context.Context,
+	commitment CommitmentType,
+) (out solana.PublicKey, err error) {
+	params := []interface{}{}
+	if commitment != "" {
+		params = append(params, M{"commitment": commitment})
+	}
+	err = cl.rpcClient.CallFor(&out, "getSlotLeader", params)
+	return
+}
