1af934f700a6bb64698541d6890a6e992a75b852
Slavomir <gagliardetto@users.noreply.github.com>
2021-07-01 22:31:18 +0200

    Add RPC/getSupply

---
+++ rpc/getSupply.go
@@ -0,0 +1,35 @@
+package rpc
+
+import (
+	"context"
+
+	bin "github.com/dfuse-io/binary"
+	"github.com/dfuse-io/solana-go"
+)
+
+type GetSupplyResult struct {
+	RPCContext
+	Value *SupplyResult `json:"value"`
+}
+
+type SupplyResult struct {
+	Total                  bin.Uint64         `json:"total"`                  // Total supply in lamports
+	Circulating            bin.Uint64         `json:"circulating"`            // Circulating supply in lamports
+	NonCirculating         bin.Uint64         `json:"nonCirculating"`         // Non-circulating supply in lamports
+	NonCirculatingAccounts []solana.PublicKey `json:"nonCirculatingAccounts"` // an array of account addresses of non-circulating accounts, as strings
+}
+
+// GetSupply returns information about the current supply.
+func (cl *Client) GetSupply(
+	ctx context.Context,
+	commitment CommitmentType,
+) (out *GetSupplyResult, err error) {
+	params := []interface{}{}
+	if commitment != "" {
+		params = append(params,
+			M{"commitment": commitment},
+		)
+	}
+	err = cl.rpcClient.CallFor(&out, "getSupply", params)
+	return
+}
