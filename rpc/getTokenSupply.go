e24be63947e522895fef2a3538c4aed48ec8580b
Slavomir <gagliardetto@users.noreply.github.com>
2021-07-02 13:50:14 +0200

    Add RPC/getTokenSupply

---
+++ rpc/getTokenSupply.go
@@ -0,0 +1,28 @@
+package rpc
+
+import (
+	"context"
+
+	"github.com/dfuse-io/solana-go"
+)
+
+type GetTokenSupplyResult struct {
+	RPCContext
+	Value *UiTokenAmount `json:"value"`
+}
+
+// GetTokenSupply returns the total supply of an SPL Token type.
+func (cl *Client) GetTokenSupply(
+	ctx context.Context,
+	tokenMint solana.PublicKey, // Pubkey of token Mint to query
+	commitment CommitmentType,
+) (out *GetTokenSupplyResult, err error) {
+	params := []interface{}{tokenMint}
+	if commitment != "" {
+		params = append(params,
+			M{"commitment": commitment},
+		)
+	}
+	err = cl.rpcClient.CallFor(&out, "getTokenSupply", params)
+	return
+}
