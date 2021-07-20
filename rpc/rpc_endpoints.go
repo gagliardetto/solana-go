58f43f60bd0cb4b1a2f15af1c1cf72c8be870541
Slavomir <gagliardetto@users.noreply.github.com>
2021-07-20 17:38:14 +0200

    Add list of default RPC endpoints

---
+++ rpc/rpc_endpoints.go
@@ -0,0 +1,10 @@
+package rpc
+
+// See more: https://docs.solana.com/cluster/rpc-endpoints
+
+const (
+	EndpointDevNet           = "https://api.devnet.solana.com"
+	EndpointTestNet          = "https://api.testnet.solana.com"
+	EndpointMainNetBeta      = "https://api.mainnet-beta.solana.com"
+	EndpointMainNetBetaSerum = "https://solana-api.projectserum.com"
+)
