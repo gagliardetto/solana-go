7c9a344746429533bbf561e8dde09eba3bc225cd
Slavomir <gagliardetto@users.noreply.github.com>
2021-08-11 17:02:20 +0200

    Add rpc/errors

---
+++ rpc/errors.go
@@ -0,0 +1,11 @@
+package rpc
+
+// rpc error:
+// - https://github.com/solana-labs/solana/blob/d5961e9d9f005966f409fbddd40c3651591b27fb/client/src/rpc_custom_error.rs
+
+// transaction error:
+// - https://github.com/solana-labs/solana/blob/8817f59b6e5ce26f578b0834dbfac5c7daa82fab/sdk/src/transaction.rs
+// - https://github.com/solana-labs/solana/blob/899b09872bd7c4af4d1c9c73f7b26d268af276eb/storage-proto/proto/transaction_by_addr.proto
+
+// instruction error
+// - https://github.com/solana-labs/solana/blob/f6371cce176d481b4132e5061262ca015db0f8b1/sdk/program/src/instruction.rs
