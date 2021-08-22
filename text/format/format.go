bacc6bc74b06f4b3174c8b6941c9b7020e44f00c
Slavomir <gagliardetto@users.noreply.github.com>
2021-08-22 16:31:26 +0200

    Fix text formatters

--- text/format/format.go
+++ text/format/format.go
@@ -1,21 +1,22 @@
 package format
 
 import (
+	"github.com/davecgh/go-spew/spew"
 	"github.com/gagliardetto/solana-go"
 	"github.com/gagliardetto/solana-go/text"
 	. "github.com/gagliardetto/solana-go/text"
 )
 
 func Program(name string, programID solana.PublicKey) string {
-	return IndigoBG("Program") + ": " + Bold("System") + " " + text.ColorizeBG(programID.String())
+	return IndigoBG("Program") + ": " + Bold(name) + " " + text.ColorizeBG(programID.String())
 }
 
 func Instruction(name string) string {
-	return Purple(Bold("Instruction")) + ": " + Bold("Transfer")
+	return Purple(Bold("Instruction")) + ": " + Bold(name)
 }
 
 func Param(name string, value interface{}) string {
-	return Sf(CC(Shakespeare(name), ": %s"), Lime(S(value)))
+	return Sf(CC(Shakespeare(name), ": %s"), Lime(spew.Sdump(value)))
 }
 
 func Account(name string, pubKey solana.PublicKey) string {
