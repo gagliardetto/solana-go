8d9c6f54e73cce49a2e3efe3628473eb0f201ba4
gagliardetto <gagliardetto@users.noreply.github.com>
2022-11-24 00:28:00 +0100

    Refactor sendandconfirmtransaction

--- rpc/sendAndConfirmTransaction/sendAndConfirmTransaction.go
+++ rpc/sendAndConfirmTransaction/sendAndConfirmTransaction.go
@@ -17,6 +17,7 @@ package sendandconfirmtransaction
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/gagliardetto/solana-go"
 	"github.com/gagliardetto/solana-go/rpc"
@@ -31,7 +32,7 @@ func SendAndConfirmTransaction(
 	transaction *solana.Transaction,
 ) (signature solana.Signature, err error) {
 	opts := rpc.TransactionOpts{
-		SkipPreflight: false,
+		SkipPreflight:       false,
 		PreflightCommitment: rpc.CommitmentFinalized,
 	}
 
@@ -41,8 +42,33 @@ func SendAndConfirmTransaction(
 		wsClient,
 		transaction,
 		opts,
+		nil,
 	)
 }
+
+func SendAndConfirmTransactionWithTimeout(
+	ctx context.Context,
+	rpcClient *rpc.Client,
+	wsClient *ws.Client,
+	transaction *solana.Transaction,
+	timeout time.Duration,
+) (signature solana.Signature, err error) {
+	opts := rpc.TransactionOpts{
+		SkipPreflight:       false,
+		PreflightCommitment: rpc.CommitmentFinalized,
+	}
+
+	return SendAndConfirmTransactionWithOpts(
+		ctx,
+		rpcClient,
+		wsClient,
+		transaction,
+		opts,
+		&timeout,
+	)
+}
+
+var ErrTimeout = fmt.Errorf("timeout")
 
 // Send and wait for confirmation of a transaction.
 func SendAndConfirmTransactionWithOpts(
@@ -51,9 +77,9 @@ func SendAndConfirmTransactionWithOpts(
 	wsClient *ws.Client,
 	transaction *solana.Transaction,
 	opts rpc.TransactionOpts,
-) (signature solana.Signature, err error) {
-
-	sig, err := rpcClient.SendTransactionWithOpts(
+	timeout *time.Duration,
+) (sig solana.Signature, err error) {
+	sig, err = rpcClient.SendTransactionWithOpts(
 		ctx,
 		transaction,
 		opts,
@@ -61,25 +87,58 @@ func SendAndConfirmTransactionWithOpts(
 	if err != nil {
 		return sig, err
 	}
+	_, err = WaitForConfirmation(
+		ctx,
+		wsClient,
+		sig,
+		timeout,
+	)
+	return sig, err
+}
 
+// WaitForConfirmation waits for a transaction to be confirmed.
+// If the transaction was confirmed, but it failed while executing (one of the instructions failed),
+// then this function will return an error (true, error).
+// If the transaction was confirmed, and it succeeded, then this function will return nil (true, nil).
+func WaitForConfirmation(
+	ctx context.Context,
+	wsClient *ws.Client,
+	sig solana.Signature,
+	timeout *time.Duration,
+) (confirmed bool, err error) {
 	sub, err := wsClient.SignatureSubscribe(
 		sig,
 		rpc.CommitmentFinalized,
 	)
 	if err != nil {
-		return sig, err
+		return false, err
 	}
 	defer sub.Unsubscribe()
 
+	if timeout == nil {
+		t := 2 * time.Minute // random default timeout
+		timeout = &t
+	}
+
 	for {
-		got, err := sub.Recv()
-		if err != nil {
-			return sig, err
-		}
-		if got.Value.Err != nil {
-			return sig, fmt.Errorf("transaction confirmation failed: %v", got.Value.Err)
-		} else {
-			return sig, nil
+		select {
+		case <-ctx.Done():
+			return false, ctx.Err()
+		case <-time.After(*timeout):
+			return false, ErrTimeout
+		case resp, ok := <-sub.Response():
+			if !ok {
+				return false, fmt.Errorf("subscription closed")
+			}
+			if resp.Value.Err != nil {
+				// The transaction was confirmed, but it failed while executing (one of the instructions failed).
+				return true, fmt.Errorf("confirmed transaction with execution error: %v", resp.Value.Err)
+			} else {
+				// Success! Confirmed! And there was no error while executing the transaction.
+				return true, nil
+			}
+		case err := <-sub.Err():
+			return false, err
 		}
 	}
 }
