ee1db61d3200e219e9f4b0b3819460b09364fc3f
imalic3 <manovisut.ktp@gmail.com>
2023-02-17 15:20:23 +0700

    tests

---
+++ programs/compute-budget/instruction_test.go
@@ -0,0 +1,67 @@
+// Copyright 2021 github.com/gagliardetto
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package computebudget
+
+import (
+	"encoding/hex"
+	"testing"
+
+	bin "github.com/gagliardetto/binary"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDecodeInstruction(t *testing.T) {
+	tests := []struct {
+		name              string
+		hexData           string
+		expectInstruction *Instruction
+	}{
+		{
+			name:    "SetComputeUnitLimit",
+			hexData: "02c05c1500",
+			expectInstruction: &Instruction{
+				BaseVariant: bin.BaseVariant{
+					TypeID: bin.TypeIDFromUint8(2),
+					Impl: &SetComputeUnitLimit{
+						Units: 1400000,
+					},
+				},
+			},
+		},
+		{
+			name:    "SetComputeUnitPrice",
+			hexData: "03e803000000000000",
+			expectInstruction: &Instruction{
+				BaseVariant: bin.BaseVariant{
+					TypeID: bin.TypeIDFromUint8(3),
+					Impl: &SetComputeUnitPrice{
+						MicroLamports: 1000,
+					},
+				},
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			data, err := hex.DecodeString(test.hexData)
+			require.NoError(t, err)
+			var instruction *Instruction
+			err = bin.NewBinDecoder(data).Decode(&instruction)
+			require.NoError(t, err)
+			assert.Equal(t, test.expectInstruction, instruction)
+		})
+	}
+}
