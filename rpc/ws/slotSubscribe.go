e5fb60ee9cc7a66e7d841788aba29b8611c7b0cc
GoudanWoo <155265132+GoudanWoo@users.noreply.github.com>
2024-03-21 11:09:53 +0800

    feat: Add `.Response()` and `.Err()` method for all subscriber

--- rpc/ws/slotSubscribe.go
+++ rpc/ws/slotSubscribe.go
@@ -54,6 +54,23 @@ func (sw *SlotSubscription) Recv() (*SlotResult, error) {
 	}
 }
 
+func (sw *SlotSubscription) Err() <-chan error {
+	return sw.sub.err
+}
+
+func (sw *SlotSubscription) Response() <-chan *SlotResult {
+	typedChan := make(chan *SlotResult, 1)
+	go func(ch chan *SlotResult) {
+		// TODO: will this subscription yield more than one result?
+		d, ok := <-sw.sub.stream
+		if !ok {
+			return
+		}
+		ch <- d.(*SlotResult)
+	}(typedChan)
+	return typedChan
+}
+
 func (sw *SlotSubscription) Unsubscribe() {
 	sw.sub.Unsubscribe()
 }
