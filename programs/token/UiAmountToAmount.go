6f0b8c52ad0da13a136b6062a0adda8e0fa28bb3
Jaroslaw Diuwe <jaroslaw.diuwe@alluvial.finance>
2025-03-13 20:35:17 +0000

    update license

--- programs/token/UiAmountToAmount.go
+++ programs/token/UiAmountToAmount.go
@@ -1,4 +1,5 @@
 // Copyright 2021 github.com/gagliardetto
+// Copyright 2025 github.com/liquid-collective
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
